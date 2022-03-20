ede8b7e4f63b1a4cf07de41c4547659d3854295b
haesoo <wjdgotn77@gmail.com>
2022-03-20 18:27:36 +0900

    Add: 실행컨텍스트란?

--- JavaScript/코어자바스크립트/실행컨텍스트/실행컨텍스트.js
+++ JavaScript/코어자바스크립트/실행컨텍스트/실행컨텍스트.js
@@ -4,13 +4,13 @@ var a = 1;
 
 function outer() {
   function inner() {
-    console.log(a);
+    console.log('inner', a);
     var a = 3;
   }
 
   inner();
-  console.log(a);
+  console.log('outer', a);
 }
 
 outer();
-console.log(a);
+console.log('전역', a);
