f44b0bdef0a856d3d22a1bf60227d6772ae3b205
maeve <wjdgotn77@gmail.com>
2022-05-20 12:57:07 +0900

    Add: 타입스크립트로 HTML 변경 및 조작 내용 추가

---
+++ TypeScript-learn/html.js
@@ -0,0 +1,49 @@
+var $title = document.querySelector("#title");
+// Error 이유: 셀렉터로 html을 찾으면 타입이 Element | null 이기 떄문에 HTML 을 못 찾을 경우 null이 된다.
+//$title.innerHTML = "반갑소";
+// Narrowing 으로 해결.
+if ($title !== null) {
+  $title.innerHTML = "Narrowing";
+}
+// instanceof 사용하는 Narrowing. => 가장 좋은 방법.
+if ($title instanceof HTMLElement) {
+  $title.innerHTML = "Instanceof";
+}
+// 또는 Assertion. => 비추
+var $title2 = document.querySelector("#title");
+$title2.innerHTML = "Assertion";
+// 또는 optional chaining 연산자.
+if (
+  ($title === null || $title === void 0 ? void 0 : $title.innerHTML) !==
+  undefined
+) {
+  $title.innerHTML = "옵셔널체이닝";
+}
+var $link = document.querySelector("#link");
+// Error
+// if ($link instanceof HTMLElement) {
+if ($link instanceof HTMLAnchorElement) {
+  $link.href = "https://kakao.com";
+}
+// html 태그 종류별로 정확한 타입명칭이 있다.
+// a Tag HTMLAnchorElement
+// img Tag HTMLImageElement
+// H4 Tag HTMLHeadingElement
+// ... 등등 정확한 타입으로 Narrowing 해줘야 html 속성 수정을 제대로 할 수 있다.
+// EventListener 부착.
+var $button = document.getElementById("button");
+$button === null || $button === void 0
+  ? void 0
+  : $button.addEventListener("click", function () {
+      console.log("hi");
+    });
+var $image = document.querySelector("#image");
+if ($image instanceof HTMLImageElement) {
+  $image.src = "new.jpg";
+}
+var $aTag = document.querySelectorAll(".naver");
+$aTag.forEach(function (el) {
+  if (el instanceof HTMLAnchorElement) {
+    el.href = "kakao.com";
+  }
+});
